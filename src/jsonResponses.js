92fda17c64106f21a80134906adfc3ca1b47b072
BenThorn <brt4557@g.rit.edu>
2018-09-25 13:14:45 -0400

    added post

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -10,6 +10,15 @@ const respondJSON = (requestAnimationFrame, response, status, object) => {
   response.end();
 };
 
+const respondJSONMeta = (request, response, status) => {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+
+  response.writeHead(status, headers);
+  response.end();
+};
+
 const getUsers = (request, response) => {
   const responseJSON = {
     users,
@@ -18,6 +27,54 @@ const getUsers = (request, response) => {
   return respondJSON(request, response, 200, responseJSON);
 };
 
+const getUsersMeta = (request, response) => respondJSONMeta(request, response, 200);
+
+const notFound = (request, response) => {
+  const responseJSON = {
+    message: 'The page you are looking for was not found.',
+    id: 'notFound',
+  };
+
+  respondJSON(request, response, 404, responseJSON);
+};
+
+const addUser = (request, response, body) => {
+  const responseJSON = {
+    message: 'Name and age are both required.',
+  };
+
+  if (!body.name || !body.age) {
+    responseJSON.id = 'missingParams';
+    return respondJSON(request, response, 400, responseJSON);
+  }
+
+  let responseCode = 201;
+
+  if (users[body.name]) {
+    responseCode = 204;
+  } else {
+    users[body.name] = {};
+  }
+
+  users[body.name].name = body.name;
+  users[body.name].age = body.age;
+
+  if (responseCode === 201) {
+    responseJSON.message = 'Created Successfully';
+    return respondJSON(request, response, responseCode, responseJSON);
+  }
+
+  return respondJSONMeta(request, response, responseCode);
+};
+
+const notFoundMeta = (request, response) => {
+  respondJSONMeta(request, response, 404);
+};
+
 module.exports = {
   getUsers,
+  getUsersMeta,
+  notFound,
+  notFoundMeta,
+  addUser,
 };
